59a67e93f1123af238296c215d4a2db7119fc328
suhasinipatil <suhasinibpatil09@gmail.com>
2023-02-21 13:07:19 +0530

    added custom methods in repository layer

--- src/main/java/com/example/springtaskmgrv2/respository/TasksRepository.java
+++ src/main/java/com/example/springtaskmgrv2/respository/TasksRepository.java
@@ -2,8 +2,21 @@ package com.example.springtaskmgrv2.respository;
 
 import com.example.springtaskmgrv2.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface TasksRepository extends JpaRepository<TaskEntity, Integer> {
+
+    List<TaskEntity> findAllByCompleted(boolean completed);
+
+    List<TaskEntity> findAllByCompletedAndDueDateBefore(boolean completed, Date dueDate);
+
+    @Query("select t from tasks t where t.completed = false and t.dueDate < current_date ")
+    List<TaskEntity> findAllOverdue();
+
+    List<TaskEntity> findAllByTitleContainingIgnoreCase(String titleFragment);
 }
--- src/test/java/com/example/springtaskmgrv2/repositories/TasksRepositoryTests.java
+++ src/test/java/com/example/springtaskmgrv2/repositories/TasksRepositoryTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
@@ -21,8 +22,42 @@ public class TasksRepositoryTests {
         task.setTitle("test task");
         task.setDescription("test description");
         task.setCompleted(false);
-        //task.setDueDate(new Date("21-02-2023"));
         var savedTask = tasksRepository.save(task);
         assertNotNull(savedTask);
+    }
+
+    @Test
+    public void testfindAllCompleted(){
+        TaskEntity task = new TaskEntity();
+        task.setTitle("test task");
+        task.setDescription("test description");
+        task.setCompleted(false);
+        TaskEntity taskEntity = new TaskEntity();
+        taskEntity.setTitle("test task");
+        taskEntity.setDescription("test description");
+        taskEntity.setCompleted(true);
+        tasksRepository.save(task);
+        tasksRepository.save(taskEntity);
+        var incompletedtasks = tasksRepository.findAllByCompleted(true);
+        assertEquals(1, incompletedtasks.size());
+        var completedtasks = tasksRepository.findAllByCompleted(false);
+        assertEquals(1, completedtasks.size());
+    }
+
+    @Test
+    public  void testGetAllTasks(){
+        TaskEntity task = new TaskEntity();
+        task.setTitle("test task");
+        task.setDescription("test description");
+        task.setCompleted(false);
+        TaskEntity taskEntity = new TaskEntity();
+        taskEntity.setTitle("test task");
+        taskEntity.setDescription("test description");
+        taskEntity.setCompleted(true);
+        tasksRepository.save(task);
+        tasksRepository.save(taskEntity);
+        var alltasks = tasksRepository.findAll();
+        assertNotNull(alltasks);
+        assertEquals(2, alltasks.size());
     }
 }
