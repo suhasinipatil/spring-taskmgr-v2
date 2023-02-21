c3f7926f5018c726cc4538872ebbbd5df1e39338
suhasinipatil <suhasinibpatil09@gmail.com>
2023-02-21 12:24:12 +0530

    added relationship between notes and tasks

--- src/main/java/com/example/springtaskmgrv2/entities/NoteEntity.java
+++ src/main/java/com/example/springtaskmgrv2/entities/NoteEntity.java
@@ -1,13 +1,13 @@
 package com.example.springtaskmgrv2.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "notes")
 public class NoteEntity extends BaseEntity{
 
     @Column(name = "body", nullable = false, length = 500)
     String body;
+
+    @ManyToOne
+    TaskEntity task;
 }
